b4b9c18f2a60f7a549a9f1dfc497f49fefaf5075
smagwaza023 <smagwaza023@student.wethinkcode.co.za>
2024-02-22 22:03:56 +0200

    initialised function and added preliminary variables

---
+++ awe.py
@@ -0,0 +1,6 @@
+#password should have a minimum length, if it should have a special characters an option will be prompted to user
+#as well as numbers within the password
+
+import string
+
+print(string.punctuation)
--- password_gen.py
+++ password_gen.py
@@ -1,3 +1,11 @@
 import string
 import random 
 
+def generate(min_length, numbers=True, special_characters=True) -> str:
+    password = ""
+    characters = string.ascii_letters
+    numbers = string.digits
+    special_chars = string.punctuation
+
+    for letter in range(min_length):
+        for 
