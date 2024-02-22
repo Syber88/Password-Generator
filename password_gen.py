70c0ddeafd54cb173bd6a626032ba2e540cf113d
smagwaza023 <smagwaza023@student.wethinkcode.co.za>
2024-02-22 23:01:53 +0200

    program works as expected and all password combinations generate

--- password_gen.py
+++ password_gen.py
@@ -1,9 +1,14 @@
 import string
 import random 
 
+
 def get_minimum_length() -> int:
-    length = int(input("what is the length of the password"))
-    return length
+    while True:
+        length = input("what is the length of the password:\n")
+        if not length.isdigit():
+            print("Please enter a digit to proceed")
+            continue
+        return int(length)  
 
 
 def numbers_inpassword() -> bool:
@@ -11,21 +16,33 @@ def numbers_inpassword() -> bool:
     return nums
 
 
-def numbers_inpassword() -> bool:
+def special_inpassword() -> bool:
     special = input("should the password include special characters. yes/no\n").lower() == "yes"
     return special
 
 
-def generate(min_length=5, numbers=True, special_characters=True) -> str:
+def generate(min_length=8, numbers=True, special_characters=True) -> str:
     str = ""
     str += string.ascii_letters
-    str += string.digits
-    str += string.punctuation
+    if numbers:
+        str += string.digits
+    if special_characters:
+        str += string.punctuation
     password = ""
 
     while len(password) < min_length:
         password += random.choice(str)
+    print("==============")
+    print(password)
+    print("==============")
     return password
+
+
+if __name__ == "__main__":
+    pass_length = get_minimum_length()
+    include_numbers = numbers_inpassword()
+    include_special = special_inpassword()
+    generate(pass_length, include_numbers, include_special)
 
 
 
