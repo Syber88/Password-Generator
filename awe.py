eab6da2be2b4c4c2dc8fbb11bd043211651bd78a
smagwaza023 <smagwaza023@student.wethinkcode.co.za>
2024-02-22 22:28:19 +0200

    added functions for number and special character cases

--- awe.py
+++ awe.py
@@ -3,4 +3,17 @@
 
 import string
 
-print(string.punctuation)
+# print(string.punctuation)
+
+# str = ""
+# str += string.ascii_letters
+# str += string.digits
+# str += string.punctuation
+
+# print(str)
+def numbers_inpassword() -> bool:
+    nums = input("should the password include numbers. yes/no\n").lower() == "yes"
+    print(nums)
+    return nums
+
+numbers_inpassword()
--- password_gen.py
+++ password_gen.py
@@ -1,11 +1,29 @@
 import string
 import random 
 
-def generate(min_length, numbers=True, special_characters=True) -> str:
-    password = ""
-    characters = string.ascii_letters
-    numbers = string.digits
-    special_chars = string.punctuation
+def get_minimum_length() -> int:
+    length = int(input("what is the length of the password"))
+    return length
 
-    for letter in range(min_length):
-        for 
+
+def numbers_inpassword() -> bool:
+    nums = input("should the password include numbers. yes/no\n").lower() == "yes"
+    return nums
+
+
+def numbers_inpassword() -> bool:
+    special = input("should the password include special characters. yes/no\n").lower() == "yes"
+    return special
+
+
+def generate(min_length, numbers=True, special_characters=True) -> str:
+    str = ""
+    str += string.ascii_letters
+    str += string.digits
+    str += string.punctuation
+    password = ""
+
+    while len(password) < min_length:
+
+
+        
