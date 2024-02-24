06ebb376acde425050e08dd3fb283ca3008c419a
smagwaza023 <smagwaza023@student.wethinkcode.co.za>
2024-02-24 20:40:46 +0200

    added validity function for the password altered generate function to be recursive and keep on generating the password if not valid

--- password_gen.py
+++ password_gen.py
@@ -49,30 +49,37 @@ def string_selection(numbers: bool=True, special_characters: bool=True) -> str:
 
 def check_valid(numbers, special_characters, password) -> bool:
     # string_select = string_selection(numbers, special_characters)
-    valid = False
+    numbers_check = False
+    special_chars_check = False
     # candidate = random.choice(string_select)
-    if numbers and any(digit.isdigit for digit in password):
-        valid = True
-    if special_characters and any(digit in string.punctuation for digit in password):
-        valid = True
+    if any(digit.isdigit() for digit in password):
+        numbers_check = True
+    if any(digit in string.punctuation for digit in password):
+        special_chars_check = True
+    if numbers_check == numbers and special_chars_check == special_characters:
+        print("ishaye khona")
+        return True
+
 
 
 def generate(min_length: int, numbers: bool, special_characters: bool) -> str:
     string_select = string_selection(numbers, special_characters)
     password = ""
-    valid = False
+
     while len(password) < min_length:
-
-
+        candidate = random.choice(string_select)
         password += candidate
 
+    valid = check_valid(numbers, special_characters, password)
     if valid:
         return password
+    else:
+        return generate(min_length, numbers, special_characters)
 
 
 if __name__ == "__main__":
     pass_length = get_minimum_length()
-    include_numbers = numbers_inpassword()
+    include_numbers = False
     include_special = special_inpassword()
     print(generate(pass_length, include_numbers, include_special))
 
